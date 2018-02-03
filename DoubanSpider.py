6056f55ac6f4aabf2c2554ef3d0d95f1e0b94dca
cirno99 <1563800293@qq.com>
2018-02-03 22:17:17 +0800

    first commit

---
+++ DoubanSpider.py
@@ -0,0 +1,118 @@
+import requests
+from bs4 import BeautifulSoup
+from os import remove
+from PIL import Image
+import pickle
+import json
+import random
+import string
+#import http.cookiejar as cookielib
+url = 'https://www.douban.com/'
+login_url='https://www.douban.com/login'
+data={'source':None,
+      'remember':'on'
+    }
+
+headers = {'Host':'www.douban.com',
+           'Referer': 'https://www.douban.com/',
+           'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+           'Accept-Encoding':'gzip, deflate, br'}
+
+Session=requests.session()
+# 读取是否保存有cookie
+try:
+    with open('cookie','rb') as f:
+        Session.cookies=pickle.load(f)
+        #这里我将读到的cookie输出
+#        print(Session.cookies)
+        log = True
+# 如果没有cookie，就读入用户输入
+except:
+    data['form_email']=input('请输入你的账户：')
+    data['form_password']=input('请输入你的密码：')
+    log = False
+
+# 获得登陆界面的验证码
+def get_captcha():
+    req=Session.get(login_url)
+    page_bf=BeautifulSoup(req.text,'html.parser')
+    # 寻找验证码的图片（有可能不需要验证码 这时返回NULL）
+    try:
+        img_src=page_bf.find('img',id='captcha_image').get('src')
+    except:
+        return 'NULL','NULL'
+    # 如果需要验证码下载该验证码并打开
+    img=requests.get(img_src)
+    if img.status_code==200:
+        with open('captcha.jpg','wb') as f:
+            f.write(img.content)
+    image=Image.open('captcha.jpg')
+    image.show()
+    # 让用户根据验证码图片输入验证码
+    captcha=input('请输入验证码：')
+    remove('captcha.jpg')
+    # 由于post-data里还要求captcha-id所以我从图片网址中截取id
+    captcha_id=img_src[img_src.find('=')+1:]
+    captcha_id=captcha_id[:captcha_id.find('&')]
+    return captcha,captcha_id
+
+def login():
+    #获得验证码和验证码id
+    if not log:
+        captcha,captcha_id=get_captcha()
+        if captcha!='NULL':
+            data['captcha-solution']=captcha
+            data['captcha-id']=captcha_id
+        # 进行登陆
+        page_login=Session.post(login_url,data=data,headers=headers)
+        # 为了验证是否登陆成功我将登陆返回的页面html打印出来发现登陆失败
+        #print(page_login.text)
+    else:
+        page_login=Session.get(url,headers=headers)
+    page_login_bf=BeautifulSoup(page_login.text,'html.parser')
+    # 如果登陆打印登录账号
+    name=page_login_bf.find_all('a',class_='bn-more')
+    print(name[0].text) 
+    # 将此时的cookie保存方便下次登陆
+    with open('cookie', 'wb') as f:
+        pickle.dump(Session.cookies,f)
+def random_bid():
+	cookie_name = 'bid'
+	#随机bid
+	cookie_value =  "".join(random.sample(string.ascii_letters + string.digits, 11))
+	c = requests.cookies.RequestsCookieJar()  
+	c.set(cookie_name, cookie_value, path='/', domain='.douban.com')  
+	Session.cookies.update(c)
+#	print(cookie_value)
+def getMovies():
+	#获取标签
+	tags = []
+	tags_url = 'https://movie.douban.com/j/search_tags?type=tv'
+	tag_re = requests.get(tags_url,cookies=Session.cookies).json()
+	tags = tag_re['tags']
+
+	#获取详情页
+	for tag in tags:
+		i = 0
+		j = 0
+		movies = []
+		while True:
+			detail_url = 'https://movie.douban.com/j/search_subjects?type=tv&tag='+ tag +'&sort=recommend&page_limit=20&page_start={}'.format(str(i))
+			details = requests.get(detail_url,cookies = Session.cookies).json()		
+			result = details['subjects']
+			for item in result:
+				movies.append(item)
+			if len(details['subjects']) == 0:
+				random_bid()
+				break
+			i += 20
+		with open('./'+tag+'.json','w') as m:
+			json.dump(movies, m)
+#random_bid()
+if __name__=='__main__':
+#	login()
+	getMovies()
+
+
---
+++ README.md
@@ -0,0 +1,31 @@
+ Douban-Spider
+=====================
+这是一只使用Python3编写的简易的豆瓣爬虫
+-----------
+
+### 简介：
+
+* 使用[Requests](http://www.python-requests.org/en/master/)模拟HTTP请求/响应，[Beautiful Soup 4](https://www.crummy.com/software/BeautifulSoup/)提取页面信息。
+* PIL库中的Image方法来获取验证码，且需要人工验证。
+* 依照知乎上*@笑虎*的方法[随机生成Cookie](https://zhuanlan.zhihu.com/p/24035574)
+* 用json文件存储数据。
+* 该爬虫参考了一些大佬的代码！
+
+## 环境依赖
+* BeautifulSoup4
+* Requests
+* Pillow
+
+## 安装
+``` shell
+$ git clone https://github.com/cirno99/Douban-Spider.git
+$ cd Douban-Spider
+$ pip install -r requirements.txt
+```
+## 使用说明
+* 先在代码中调用login函数生成名为cookie的pickle文件
+* 注释login函数后，再调用getMovies函数进行爬取
+``` shell
+$ python3 DoubanSpider.py
+```
+* 默认为爬取电视剧数据。若想爬取电影数据，需更改*"tags_url"*和*"detail_url"*中"type=tv"为"type=movie"
